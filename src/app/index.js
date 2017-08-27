8655877adf02f0be313a9fc88f6a8b9edb127902
Quyền Anh <quyenanh198@gmail.com>
2017-08-27 10:37:11 -0700

    States

---
+++ src/app/index.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Create component
+var TodoComponent = React.createClass({
+    render:function () {
+        return(
+            <div>
+                <p><strong>Cheese name: </strong> {this.props.cheese.name}</p>
+                <p><strong>Cheese smell factor: </strong> {this.props.cheese.smellFactor}</p>
+                <p><strong>Cheese price: </strong> {this.props.cheese.price}</p>
+            </div>
+        );
+    }
+});
+
+var myCheese = {name: 'Camembert', smellFactor: 'Extreme pong', price: '3.5$'};
+
+//put component into html page
+ReactDOM.render(<TodoComponent cheese={myCheese} />, document.getElementById('todo-wrapper'));
